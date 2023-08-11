9865ede84e4ee343e8f1fbbf417c7906b232a866
onokajames96 <jamesonoka996@gmail.com>
2023-08-11 10:43:31 -0700

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for num in str:
+        if 'a' <= num <= 'z':
+            print(chr(ord(num) - ord('a') + ord('A')), end='')
+        else:
+            print(num, end='')
+    print()
+
+
