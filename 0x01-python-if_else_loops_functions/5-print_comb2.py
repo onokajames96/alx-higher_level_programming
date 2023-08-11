a0bca993d0a765d1f9e986e07df699916dd0b0a3
onokajames96 <jamesonoka996@gmail.com>
2023-08-11 05:16:13 -0700

    update changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        print("{:02}, ".format(number), end='' )
+        print("{:02}, ".format(number), end=', ')
     else:
         print("{:02}".format(number))
 
