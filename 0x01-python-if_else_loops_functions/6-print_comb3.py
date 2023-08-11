978e14a9a25a1b99c467759bcc8404fe8622d7f6
onokajames96 <jamesonoka996@gmail.com>
2023-08-11 09:07:13 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(10):
     for k in range(num +1, 10):
-        if num != 8 and k != 9:
+        if num != 8 or k != 9:
             print("{:02}, ".format(num * 10 + k), end='')
 print("{:02}".format(89))
