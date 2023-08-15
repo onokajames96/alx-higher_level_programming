59447e024025854788f4eb790e21605176009e17
onokajames96 <jamesonoka996@gmail.com>
2023-08-14 20:01:10 -0700

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
+    for row in matrix:
+        for num in range(len(row)):
 
+            if num == len(row) - 1:
+                print("{}".format(row[num]))
+            else:
+                print("{:d}".format(row[num]), end=" ")
