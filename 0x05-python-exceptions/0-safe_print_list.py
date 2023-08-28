14dc72a7dc2a4013e07eb981e9e848c49cc2f992
onokajames96 <jamesonoka996@gmail.com>
2023-08-28 06:07:13 -0700

    prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+
+    try:
+        for j in range(x):
+            print("{}".format(my_list[j]), end="")
+            num += 1
+    except:
+        pass
+
+    print()
+    return num
