3df4db056b87b4b4c81508b1c7f4a8a4dfa70f4a
onokajames96 <jamesonoka996@gmail.com>
2023-08-28 06:14:37 -0700

    changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,10 @@ def safe_print_list(my_list=[], x=0):
     num = 0
 
     try:
-        for j in range(x):
+        for j in range(0, x):
             print("{}".format(my_list[j]), end="")
             num += 1
     except:
         pass
-
     print()
     return num
