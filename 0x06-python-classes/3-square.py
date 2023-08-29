52784bdaa075f00bc1c907f8716722d7bc9d09cc
onokajames96 <jamesonoka996@gmail.com>
2023-08-29 08:39:17 -0700

    very challenging

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+class Square():
+    def __init__(self, size=0):
+        """Initialization """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        return (self.__size * self.__size)
