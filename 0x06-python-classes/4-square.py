202a85773ca74e1ba6ee11d6011cce34c8ad3bd8
onokajames96 <jamesonoka996@gmail.com>
2023-08-29 14:19:13 -0700

    Why a getter and setter?

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""A Class Square defines a square.
+"""
+
+class Square:
+    """ Object Square.
+    """
+    def __init__(self, size=0):
+        """ Instantiation.
+        Args:
+            
+            size(int): The size of the square.
+        """
+        self.__size = size
+    @property
+    def size(self):
+        """Retrieves it."""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """ sets it.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        """That returns the current square area.
+        """
+        return self.__size * self.__size
