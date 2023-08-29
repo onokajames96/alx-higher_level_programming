b0dfa15478c5bbd131f814235afa5edb602cdf98
onokajames96 <jamesonoka996@gmail.com>
2023-08-29 14:55:41 -0700

    update changes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,14 +32,13 @@ class Square:
         """
         return self.__size * self.__size
 
-     def my_print(self):
+    def my_print(self):
 
+        if self.__size == 0:
 
-
-         if self.__size == 0:
-             print("")
-         else:
-              for x in range(0, self.__size):
-                  for y in range(0, self.__size):
-                      print("#", end="")
-                  print()
+            print("")
+        else:
+            for x in range(0, self.__size):
+                for y in range(0, self.__size):
+                    print("#", end="")
+                print()
