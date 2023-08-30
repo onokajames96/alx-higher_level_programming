fead165fbbf069c87e68c7548672b522fb7545be
onokajames96 <jamesonoka996@gmail.com>
2023-08-30 00:17:34 -0700

    prints the alphabet in uppercase,

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,13 +9,12 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        operator = {"+": add, "-": sub, "*": mul, "/": div}
+        operators = {"+": add, "-": sub, "*": mul, "/": div}
 
-        if sys.argv[2] not in operators:
-            print("Unknown operator. Available operators: +, -, * and  /")
-            sys.exit(1)
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and  /")
+        sys.exit(1)
 
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-
-            print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
