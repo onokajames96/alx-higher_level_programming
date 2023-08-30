bd3ddf5bcc110b7a838229d6ff90e40d2fb171a6
onokajames96 <jamesonoka996@gmail.com>
2023-08-30 01:07:53 -0700

    update changes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """that adds 2 tuples"""
-    a = 0
-    b = 0
-    result = (a[0] + b[0], a[1] + b[1])
-    return result 
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    result = a[0] + b[0], a[1] + b[1]
+    return (result)
