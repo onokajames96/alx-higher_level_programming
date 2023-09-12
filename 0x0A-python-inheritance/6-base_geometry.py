d275f5bb7522cca105e0dcfcfd8c3e2d504e6320
onokajames96 <jamesonoka996@gmail.com>
2023-09-12 03:09:05 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#James Onoka
+"""a class BaseGeometry."""
+
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        instance method that raises an Exception with the message
+        "area() is not implemented."
+
+        """
+
+        raise Execption("area() is not implemented")
