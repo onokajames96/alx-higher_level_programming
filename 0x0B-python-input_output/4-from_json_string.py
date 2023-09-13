2875d01ff181d241cb06ce7e0956f868e66fb07c
onokajames96 <jamesonoka996@gmail.com>
2023-09-13 07:16:00 -0700

    JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Onoka J
+"""
+A function that returns an object (Python data structure)
+represented by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns:
+        an object (Python data structure) represented by a JSON string.
+    """
+    return json.loads(my_str)
