bfeeae3909e418cfd2960fc7eff630af2e0d560c
onokajames96 <jamesonoka996@gmail.com>
2023-09-13 06:02:21 -0700

    write mode

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+
 def write_file(filename="", text=""):
+    """
+    Write a string to a text file (UTF-8)
+    and return the number of characters written.
+    """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
