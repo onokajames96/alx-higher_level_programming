11a748eb18440299436ccaa72db9d36d4f38c09e
onokajames96 <jamesonoka996@gmail.com>
2023-09-13 06:19:37 -0700

    reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""A function that reads a Text file."""
-
+"""
+A function that reads a Text file.
+"""
 
 def read_file(filename=""):
+    """
+    Read and print the content of a text file.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
