3d793307d5dabb0ffe5fe119a92abbfecc9f6e43
onokajames96 <jamesonoka996@gmail.com>
2023-09-13 13:09:21 -0700

    froms JSON nfile

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """
+Module: 6-load_from_json_file.
 """
 import json
 
 
 def load_from_json_file(filename):
+    """
+    Load JSON data from a file and return the corresponding object.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The Python object created from the JSON data.
+    """
     with open(filename, 'r') as file:
         return json.load(file)
-
