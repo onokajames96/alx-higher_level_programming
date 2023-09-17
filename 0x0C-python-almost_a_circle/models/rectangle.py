48492db799884794a137065fd3e709dd9c95673e
onokajames96 <jamesonoka996@gmail.com>
2023-09-17 15:22:22 -0700

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,47 @@
 #!/usr/bin/python3
 
 
-class Rectangle:
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a Rectangle instance.
+
+        Args:
+            width (int): The width.
+            height (int): The height.
+            x (int): x-coordinate.
+            y (int): y-coordinate.
+            id (int, optional): ID.
+
+        Returns:
+            None
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width with validation."""
+         if not isinstance(value, int) or value <= 0:
+             raise ValueError("Width must be a positive integer")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+         """Set height with validation."""
+         if not isinstance(value, int):
+             raise ValueError("Height must be a positive integer")
+         self.__height = value
