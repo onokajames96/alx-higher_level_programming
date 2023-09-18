000708d45520f997c429cb6da36cffdc66f966b9
onokajames96 <jamesonoka996@gmail.com>
2023-09-18 10:50:10 -0700

    add tests folder

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 
 class Base:
@@ -31,3 +32,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Serialize a list of dictionaries to a JSON string.
+
+        Returns the JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return  "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
