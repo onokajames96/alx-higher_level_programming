44cb87ebedaab8a7ac42af3b3e92234bbe5ff83a
onokajames96 <jamesonoka996@gmail.com>
2023-09-21 15:03:05 -0700

    tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Write the class Rectangle that inherits from Base."""
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Initialize a Rectangle instance.
