f01f58336e7a73c6d7d751bc038fe7baa9ce4614
onokajames96 <jamesonoka996@gmail.com>
2023-09-22 02:35:06 -0700

    almost a cicrle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,8 +6,20 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-
 class TestBase(unittest.TestCase):
+    """
+    Test cases for the Base class.
+    """
     def test_init_with_id(self):
+        b = Base(5)
+        self.assertEqual(b.id, 5)
 
+    def test_init_without_id(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
 
+    def test_empty_to_json_string(self):
+        json_string = Base.to_json_string([])
