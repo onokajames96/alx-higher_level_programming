649eee50311f53433d8dbf7b03b8f0ea11080738
onokajames96 <jamesonoka996@gmail.com>
2023-09-30 02:05:05 -0700

    python classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,13 +3,14 @@
 """A Class Square defines a square.
 """
 
+
 class Square:
     """ Object Square.
     """
     def __init__(self, size=0):
         """ Instantiation.
+
         Args:
-            
             size(int): The size of the square.
         """
         self.__size = size
