7b8854fbf5a7b346ea409c9f6510c2a72df27e31
onokajames96 <jamesonoka996@gmail.com>
2023-10-01 10:57:37 -0700

    update chages

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,4 +16,4 @@ def inherits_from(obj, a_class):
         inherited from the specified class; otherwise, False.
     """
 
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
