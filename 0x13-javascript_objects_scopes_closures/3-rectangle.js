3c3f00af0eea3ca0e798ae354a695e72705d0e81
onokajames96 <jamesonoka996@gmail.com>
2023-11-14 12:01:14 -0800

    javascript objects,scope

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const array = [];
+    for (let i = 0; i < this.width; i++) {
+      array.push('w');
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(array.join(''));
+    }
+  }
+};
