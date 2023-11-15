9cf83b455959eed217686610c373895d3b55cb70
onokajames96 <jamesonoka996@gmail.com>
2023-11-15 05:39:52 -0800

    reserved list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const newList = [];
+
+  for (let index = list.length - 1; index >= 0; index--) {
+    newList.push(list[index]);
+  }
+
+  return newList;
+};
