80238b15a91e312528b85a0ecbd1663a292c86a2
onokajames96 <jamesonoka996@gmail.com>
2023-11-15 04:09:02 -0800

    Square

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,26 +4,26 @@ class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-      this.width = w;	   
+      this.width = w;
+    }
   }
- }
- 
- print () {
-   for (let index = 0; index < this.height; index++) {
-     console.log('X'.repeat(this.width));
-   }
- }
- 
- rotate () {
-   const aux = this.width;
-   this.width = this.height;
-   this.height = aux;
- }
- 
- double () {
-   this.width *= 2;
-   this.height *= 2;
- }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
