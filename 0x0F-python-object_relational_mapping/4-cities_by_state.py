de01c2fb18354f745374b5a2a78a3412d5f4a322
onokajames96 <jamesonoka996@gmail.com>
2023-11-20 01:31:24 -0800

    fixing  error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                          passwd=sys.argv[2], db=sys.argv[3])
+                        passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
                  FROM cities LEFT JOIN states\
