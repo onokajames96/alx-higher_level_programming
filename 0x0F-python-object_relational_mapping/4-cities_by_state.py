ffdc3bf1bb368e89f4f72a4b5a4934d4d5bba90c
onokajames96 <jamesonoka996@gmail.com>
2023-11-20 01:22:02 -0800

    lists cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all cities fromdatabase"""
+
+import MySQldb
+import sys
+
+
+if __name__ == '__main__':
+    db = MyWSQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities LEFT JOIN states\
+                 ON states.id = cities.state_id\
+                 ORDER BY cities.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all cities of that state."""
+import MySQLdb
+import sys
+
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT cities.name\
+            FROM cities LEFT JOIN states\
+            ON states.id = cities.state_id\
+            WHERE states.name = %s\
+            ORDER BY cities.id ASC", (sys.argv[4],))
+    row = c.fetchall()
+    print(", ".join([row[0] for row in rows]))
+
+    c.close()
+    db.close()
