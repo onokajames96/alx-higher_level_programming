4186d984c961749b12ae803c21d76d069490e1bf
onokajames96 <jamesonoka996@gmail.com>
2023-11-20 01:29:49 -0800

    fix error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """lists all cities fromdatabase"""
 
-import MySQldb
+import MySQLdb
 import sys
 
 
 if __name__ == '__main__':
-    db = MyWSQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                           passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
