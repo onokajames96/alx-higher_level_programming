a89b88a2f89f1ea8999cf0c774382fde9233726a
onokajames96 <jamesonoka996@gmail.com>
2023-12-05 06:23:42 -0800

    error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response.
+Prints an error message if the HTTP status code is greater than or equal to 400.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+
+    if r.status_code >= 400:
+        print('Error code:', r.status_code)
+    else:
+        print(r.text)
