a5f9a63a4c0079e2256c241b81ecc2aeb1217451
onokajames96 <jamesonoka996@gmail.com>
2023-12-05 04:31:15 -0800

    hbn status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """takes in a URL, sends a request to the URL."""
 import urllib.request
-from sys import argv
+import urllib.error
+import sys
 
 
 if __name__ == "__main__":
     try:
-        with request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            print(html.decode('utf-8'))
-    except error.HTTPError as err:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as err:
         print('Error code: {}'.format(err.code))
