0b9b08306f60ed36df4055523432ad293cf2f5fb
onokajames96 <jamesonoka996@gmail.com>
2024-01-16 09:56:34 -0800

    javascript

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const argv = process.argv;
+let filePath = argv[2];
+let string = argv[3];
+
+fs.writeFile(filePath, string, 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
