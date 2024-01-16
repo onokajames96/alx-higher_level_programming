d9ede57005ac3249563de0542d2a2c367783a758
onokajames96 <jamesonoka996@gmail.com>
2024-01-16 10:11:17 -0800

    javascript

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const argv = process.argv;
+const url = argv[2];
+
+request.get(url).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
